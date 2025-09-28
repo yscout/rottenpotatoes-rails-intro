93fa6b30db4c32b89de13bcb3c30a760e0b5aeba
Yutao Mao <ym3019@columbia.edu>
2025-09-28 21:41:19 +0000

    Finished the Part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,16 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    @ratings_to_show =
+      if params[:ratings].present?
+        params[:ratings].keys
+      else
+        @all_ratings
+      end
+
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  RATINGS = %w[G PG PG-13 R NC-17].freeze
+
+  def self.all_ratings
+    RATINGS
+  end
+
+  def self.with_ratings(ratings_list)
+    return all if ratings_list.blank?
+    where(rating: ratings_list)
+  end
 end
